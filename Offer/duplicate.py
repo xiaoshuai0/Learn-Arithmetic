aa483d0f000b7ca65afdc1673b23e9a501e7c519
xiaoshuai0 <zhaoshuai927939@yahoo.com>
2019-06-23 22:12:53 +0800

    不修改数组找出重复的数字

--- Offer/duplicate.py
+++ Offer/duplicate.py
@@ -22,7 +22,41 @@ def duplicate(nums):
             nums[temp] = temp
     return None
 
+# 不修改数组找出重复的数字
+# 在长度为n+1的数组里的所有数字都在1~n的范围内, 所以数组中至少有一个数字是重复的. 请找出数组中任意一个重复的数字, 但不能修改输入的数组.
+# 如果输入长度为8的数组{2, 3, 5, 4,3, 2, 6, 7}, 那么对应的输出是重复的数字2或者3.
+
+def getDuplication(nums):
+    count = len(nums)
+    if count <= 0:
+        return -1
+
+    start = 1
+    end = count - 1
+    while end >= start:
+        middle = (end - start) // 2 + start
+        result = countRange(nums, count, start, middle)
+        if end == start:
+            if result > 1:
+                return start
+            else:
+                break
+        if result > (middle - start + 1):
+            end = middle
+        else:
+            start = middle + 1
+    return -1
+
+def countRange(nums, count, start, end):
+    result = 0
+    for i in range(count):
+        if nums[i] >= start and nums[i] <= end:
+            result = result + 1
+    return result
+
 
 if __name__ == "__main__":
     result = duplicate([2, 3, 1, 0, 2, 5, 3])
     print(result)
+    result1 = getDuplication([2, 3, 5, 4, 3, 2, 6, 7])
+    print(result1)
