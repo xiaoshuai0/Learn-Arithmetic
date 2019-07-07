990a4d671900fe19b3fec211a0732aeb02c4d0c6
xiaoshuai0 <zhaoshuai927939@yahoo.com>
2019-07-07 22:10:39 +0800

    sort demo

--- Offer/sortDemo.py
+++ Offer/sortDemo.py
@@ -1,5 +1,6 @@
 class Solution(object):
 
+    # 冒泡排序时间复杂度 n^2
     def bubbleSort(self, nums):
         flag = False
         for i in range(len(nums)):
@@ -11,7 +12,7 @@ class Solution(object):
             if not flag:
                 break
         return nums
-
+    # 插入排序时间复杂度 n^2
     def insertionSort(self, nums):
         for i in range(1, len(nums)):
             value = nums[i]
@@ -25,6 +26,7 @@ class Solution(object):
             nums[j+1] = value
         return nums
 
+    # 选择排序时间复杂度 n^2
     def selectionSort(self, nums):
         for i in range(0, len(nums) - 1):
             minIndex = i
@@ -33,8 +35,44 @@ class Solution(object):
                     minIndex = j
             nums[minIndex], nums[i] = nums[i], nums[minIndex]
         return nums
+    # 归并排序时间复杂度 nlogn
+    def mergeSort(self, nums):
+        if len(nums) <= 1:
+            return nums
+        num = len(nums) // 2
+        left = self.mergeSort(nums[:num])
+        right = self.mergeSort(nums[num:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        r, l = 0, 0
+        result = []
+        while l < len(left) and r < len(right):
+            if left[l] <= right[r]:
+                result.append(left[l])
+                l += 1
+            else:
+                result.append(right[r])
+                r += 1
+        result += list(left[l:])
+        result += list(right[r:])
+        return result
+
+    def quickSort(self, nums):
+        if len(nums) <= 1: return nums
+        mid = nums[0]
+        left, right = [], []
+        nums.remove(mid)
+        for num in nums:
+            if num >= mid:
+                right.append(num)
+            else:
+                left.append(num)
+        return self.quickSort(left) + [mid] + self.quickSort(right)
 
 if __name__ == "__main__":
     # print(Solution().bubbleSort([2,9,3,4,8,3]))
     # print(Solution().insertionSort([2, 9, 3, 4, 8, 3]))
-    print(Solution().selectionSort([2, 9, 3, 4, 8, 3]))
+    # print(Solution().selectionSort([2, 9, 3, 4, 8, 3]))
+    # print(Solution().mergeSort([2, 9, 3, 4, 8, 3]))
+    print(Solution().quickSort([2, 9, 3, 4, 8, 3]))
